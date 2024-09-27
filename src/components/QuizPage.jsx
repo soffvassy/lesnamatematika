3c7b95315293bbc699c360a7c271fb05b5707be7
Soff Vassy <soffvassy@users.noreply.github.com>
2024-09-27 15:40:20 +0300

    First commit

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -2,6 +2,23 @@ import { useState, useEffect } from 'react'
 import React from 'react'
 import questiondata from '../quizzes/random-easy.json';
 
+const Question = (props) => {
+  
+  return(
+    <div data-id={props.id} className="question-container">
+      {props.questionText}
+      {props.answers ?  
+      Object.entries(props.answers).map(([key, value]) => (
+        <ul>
+          <li key={key} className={props.answered? props.answered=="correct"? "green" : "red" : ""} onClick={()=>props.handleAnswer(key, props.correctAnswer)}>
+            {key}: {value}
+          </li>
+          </ul>
+        )) : ""}
+  
+    </div>
+  )
+}
 
 function QuizPage() {
   const [data, setData] = useState([]);
